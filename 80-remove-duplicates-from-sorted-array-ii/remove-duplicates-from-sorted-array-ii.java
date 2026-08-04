31f9b0421deac116bd8e352657ce98e3c3afe5ce
AkshayaDharshini246 <akshayadharshini246@gmail.com>
2026-08-04 22:52:01 +0530

    Time: 1 ms (17.41%) | Memory: 48.7 MB (51.54%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,4 +1,5 @@
-class Solution 
+
+/*class Solution 
 {
     public int removeDuplicates(int[] nums) 
     {
@@ -30,5 +31,60 @@ class Solution
         Solution s=new Solution();
         int[]nums={1,1,1,2,2,3};
         System.out.println(s.removeDuplicates(nums));
+    }
+}
+*/
+
+
+
+
+import java.util.Scanner;
+public class Solution
+{
+    public int removeDuplicates(int[] nums) 
+    {
+        int n=nums.length;
+        int count=1;
+        int j=1;
+        int result[]=new int[n];
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]==nums[i-1])
+                count++;
+            else
+                count=1;
+            if(count<=2)
+            {
+                result[j]=nums[i];
+                j++;
+            }
+                
+        }
+        for(int i=1;i<n;i++)
+        {
+            nums[i]=result[i];
+        }
+        //System.out.print(Arrays.toString(nums));
+        return j;
+    }
+    public static void main(String[]args)
+    {
+        Solution s=new Solution();
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter N value: ");
+        int n=in.nextInt();
+
+        System.out.println();
+
+        int nums[]=new int[n];
+        System.out.print("Enter array values:");
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=in.nextInt();
+        }
+
+        System.out.println(s.removeDuplicates(nums));
+        System.out.print(Arrays.toString(nums));
+
     }
 }
